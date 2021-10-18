217e8420c63f3a6b53f1af1956160b5003556d0e
OneAuLeaf <48062933+OneAuLeaf@users.noreply.github.com>
2021-10-18 11:35:34 +0300

    Lab 1, try#1 (#1)
    Первая лабораторная

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Threading;
+using Recognition;
+
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        readonly static string ModelPath = @"../../../../YOLOV4/yolov4.onnx";
+
+        readonly static int ThreadNum = Environment.ProcessorCount;
+        // args: path_to_images [path_to_model, [threads_num]]
+        static void Main(string[] args)
+        {
+            string pathImages, modelPath;
+            int threadNum;
+            switch (args.Length)
+            {
+                case 1:
+                    pathImages = args[0];
+                    modelPath = ModelPath;
+                    threadNum = ThreadNum;
+                    break;
+                case 2:
+                    pathImages = args[0];
+                    modelPath = args[1];
+                    threadNum = ThreadNum;
+                    break;
+                case 3:
+                    pathImages = args[0];
+                    modelPath = args[1];
+                    if (!int.TryParse(args[2], out threadNum)) 
+                        threadNum = ThreadNum;
+                    break;
+                default:
+                    return;
+            }
+
+            Recognizer recognizer = new Recognizer(modelPath, threadNum);
+
+            var res = recognizer.Recognize(pathImages);
+
+            var t = Task.Factory.StartNew(() =>
+            {
+                if (Console.ReadLine().EndsWith("cc")) { // 'cc' + Enter = Cancel
+                    recognizer.Cancel();
+                }
+            },
+            TaskCreationOptions.LongRunning);
+            
+            RecognizedImage image;
+            while (!res.IsCompleted || !recognizer.ResultsQueue.IsEmpty) {
+
+                if (recognizer.ResultsQueue.TryDequeue(out image))
+                    Console.WriteLine(image.ToString("0.00"));
+                Thread.Sleep(0);
+            }
+        }
+    }
+}
---
+++ Recognition/RecognizedImage.cs
@@ -0,0 +1,85 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace Recognition
+{
+    public class RecognizedImage
+    {
+        public string ImageName { get; }
+        public string ImagePath { get; }
+        public Bitmap Bitmap { get; }
+
+        public List<RecognizedObject> Objects;
+
+        public RecognizedImage(string imageName, string imagePath, Bitmap bitmap, IReadOnlyList<YoloV4Result> objects)
+        {
+            ImageName = imageName;
+            ImagePath = imagePath;
+            Bitmap = bitmap;
+            Objects = objects?.Select(x => new RecognizedObject(x)).ToList();
+        }
+
+        public string ToString(string format)
+        {
+            return $"{ImageName} : \t\n" +
+                string.Join("\t\n", from data in Objects select data.ToString(format));
+        }
+
+        public override string ToString()
+        {
+            return ToString(null);
+        }
+
+    }
+    public class RecognizedObject
+    {
+        public string Label { get; }
+        public float Confidence { get; }
+
+        public Box Box;
+
+        public RecognizedObject(YoloV4Result res)
+        {
+            Label = res.Label;
+            Box = new Box(res.BBox[0], res.BBox[1], res.BBox[2], res.BBox[3]);
+            Confidence = res.Confidence;
+        }
+
+        public string ToString(string format)
+        {
+            return $" label={Label}, p={Confidence.ToString(format)}) " + Box.ToString(format);
+        }
+
+        public override string ToString()
+        {
+            return ToString(null);
+        }
+    }
+
+    public struct Box
+    {
+        public float X { get; private set; }
+        public float Y { get; private set; }
+        public float W { get; private set; }
+        public float H { get; private set; }
+
+        public Box(float x1, float y1, float x2, float y2)
+        {
+            X = x1;
+            Y = y1;
+            W = x2 - x1;
+            H = y2 - y1;
+        }
+
+        public string ToString(string format)
+        {
+            return $"Box : ({X.ToString(format)}; {Y.ToString(format)}; {(X + W).ToString(format)}; {(Y + H).ToString(format)})";
+        }
+
+        public override string ToString()
+        {
+            return ToString(null);
+        }
+    }
+}
